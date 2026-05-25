beb8a13b700c46cfbfeb40b0f493d2b09ecdb5f9
Carolina Ciolin <166610142+CarolinaCiolin@users.noreply.github.com>
2026-05-25 20:34:30 -0300

    add: Exercise 1

---
+++ exercise-1.py
@@ -0,0 +1,8 @@
+# Create a program that reads a temperature in Celsius and converts it to Fahrenheit.
+
+temperature_celsius = float(input("Enter temperature in Celsius: "))
+
+temperature_fahrenheit = (temperature_celsius * 9/5) + 32
+
+print(f"{temperature_celsius} degrees Celsius is equal to {temperature_fahrenheit} degrees Fahrenheit.")
+
